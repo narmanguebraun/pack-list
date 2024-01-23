452492c3b2a822999d021a48e10edb1f82303804
narmanguebraun <narmanguebraun1@gmail.com>
2024-01-24 00:55:11 +0100

    Improve homepage layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,19 @@
-import AuthForm from "@/components/AuthForm";
+import AuthForm from "@/app/components/AuthForm";
 
 export default function Home() {
   return (
-    <main className=" min-h-screen p-24">
-      <h1>Welcome to Pack</h1>
-      <p>
-        Your personal space to curate and manage a pack-list of your favorite
-        items. Sign in to create, view, edit and delete items from your
-        pack-list.
-      </p>
-      <div>
-        <AuthForm />
+    <main className="min-h-screen py-24 px-8 font-mono">
+      <div className="max-w-[600px] m-auto">
+        <h1>Welcome to Pack-List</h1>
+        <p>
+          Your personal space to curate your favorite items, and manage your
+          travel pack-list. Sign in to add, view, edit and delete items from
+          your Pack-List.
+        </p>
+
+        <div className="mt-8">
+          <AuthForm />
+        </div>
       </div>
     </main>
   );

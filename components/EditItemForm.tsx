880353bb1f7ae11702df1c4a66260a71f7d2d40d
narmanguebraun <narmanguebraun1@gmail.com>
2024-09-02 18:53:51 +0200

    Refactor Edit form

---
+++ components/EditItemForm.tsx
@@ -0,0 +1,84 @@
+"use client";
+
+import { useState, useRef } from "react";
+import { updateItem } from "@/actions/actions";
+import Button from "./ui/Button";
+
+export type Item = {
+  id: number;
+  brand: string;
+  model: string;
+  reference: string;
+};
+
+interface EditItemFormProps {
+  item: Item;
+  onSubmit?: () => void;
+}
+
+export default function EditItemForm({ item, onSubmit }: EditItemFormProps) {
+  const ref = useRef<HTMLFormElement>(null);
+  const [formData, setFormData] = useState({
+    brand: item.brand,
+    model: item.model,
+    reference: item.reference,
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  return (
+    <form
+      ref={ref}
+      action={async (formData) => {
+        ref.current?.reset();
+        await updateItem(formData);
+      }}
+      onSubmit={onSubmit}
+    >
+      <div className="flex flex-col gap-4">
+        <input type="hidden" name="id" value={item.id} />
+        <div className="flex flex-col">
+          <label htmlFor="brand" className="text-xs mb-2">
+            Brand
+          </label>
+          <input
+            className="border-b border-b-dark bg-black p-2 font-mono"
+            type="text"
+            id="brand"
+            name="brand"
+            value={formData.brand}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="flex flex-col">
+          <label htmlFor="model" className="text-xs mb-2">
+            Model
+          </label>
+          <input
+            className="border-b border-b-dark bg-black p-2 font-mono"
+            type="text"
+            id="model"
+            name="model"
+            value={formData.model}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="flex flex-col">
+          <label htmlFor="reference" className="text-xs mb-2">
+            Reference
+          </label>
+          <input
+            className="border-b border-b-dark bg-black p-2 font-mono"
+            type="text"
+            id="reference"
+            name="reference"
+            value={formData.reference}
+            onChange={handleChange}
+          />
+        </div>
+        <Button type="submit">Update</Button>
+      </div>
+    </form>
+  );
+}
---
+++ components/EditItemModal.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { useState } from "react";
+import Modal from "@/components/Modal";
+import Button from "@/components/ui/Button";
+import EditItemForm, { Item } from "./EditItemForm";
+import { EditIcon } from "./ui/Icons";
+
+interface EditItemModalProps {
+  item: Item;
+}
+
+export default function EditItemModal({ item }: EditItemModalProps) {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <>
+      <button
+        onClick={() => setOpen(true)}
+        className="flex items-center gap-2 bg-black text-white  hover:bg-white hover:text-black rounded py-1 px-2 text-xs"
+      >
+        <EditIcon /> Edit
+      </button>
+      <Modal open={open} onClose={() => setOpen(false)} title="Edit Item">
+        <EditItemForm item={item} onSubmit={() => setOpen(false)} />
+      </Modal>
+    </>
+  );
+}

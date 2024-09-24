c8c514a59f44e646ea30201f988b0efdda634924
narmanguebraun <narmanguebraun1@gmail.com>
2024-09-24 23:52:40 +0200

    Change component name

--- components/EditItem.tsx
+++ components/EditItem.tsx
@@ -3,21 +3,22 @@
 import { useState } from "react";
 import Modal from "@/components/Modal";
 import Button from "@/components/ui/Button";
-import EditItemForm, { Item } from "./EditItemForm";
+import EditItemForm from "./EditItemForm";
 import { EditIcon } from "./ui/Icons";
+import { Item } from "@/types";
 
-interface EditItemModalProps {
+interface EditItemProps {
   item: Item;
 }
 
-export default function EditItemModal({ item }: EditItemModalProps) {
+export default function EditItem({ item }: EditItemProps) {
   const [open, setOpen] = useState(false);
 
   return (
     <>
       <button
         onClick={() => setOpen(true)}
-        className="flex items-center gap-2 bg-black text-white  hover:bg-white hover:text-black rounded py-1 px-2 text-xs"
+        className="flex items-center gap-2 bg-dark text-white  hover:bg-white hover:text-black rounded py-1 px-2 text-xs"
       >
         <EditIcon />
       </button>
